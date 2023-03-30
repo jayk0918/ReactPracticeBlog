89fa28e985749024294e8c468099d07add7ec0b0
jayk0918 <jesper2able@gmail.com>
2023-03-31 00:00:21 +0900

    React Introduction 22.03.31 00:00

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+
+  let posts = '강남 고기맛집';
+  let style = { color : 'yellow', fontSize : '30px' };
+
+  function number(){
+    return 1000;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div style={ style }>React 연습 blog</div>
+      </div>
+      <h4>{ posts }</h4>
+      <div className = {posts}>{ number() }</div>
+      <img src={ logo } alt = "sample"></img>
     </div>
   );
 }
